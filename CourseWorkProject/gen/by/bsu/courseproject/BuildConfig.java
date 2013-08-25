adc0956806e879928f22e45a634ddfa538973aaf
MazialiukD <mdarijan@yandex.ru>
2013-08-25 23:04:09 +0300

    proper .gitignore

--- CourseWorkProject/gen/by/bsu/courseproject/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package by.bsu.courseproject;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
